bd87a7978e43b647cc15b89c46965bea3dd99824
bogdan-lab <lakatosh.b.v@gmail.com>
2023-09-03 20:01:54 +0200

    Working selector without logging

--- selector.py
+++ selector.py
@@ -1,27 +1,77 @@
 import os
 import random
 from flask import Flask, render_template, request, redirect, url_for
+from pathlib import Path
+from time import time
+import random
+from collections import namedtuple
+
 
 app = Flask(__name__)
 
-# List of sample image URLs
-image_urls = [
-    "images/angle_1693733578_0.png",
-    "images/angle_1693733578_1.png",
-    "images/angle_1693733578_2.png",
-]
+def get_files_in_folder(path: Path, ext: str):
+    file_names = os.listdir(path)
+    result = []
+    for el in file_names:
+        if el.endswith(ext):
+            result.append(Path(path, el))
+    return result
 
-# Initialize the current images
-current_images = random.sample(image_urls, 2)
+
+FileIndex = namedtuple("FileIndex", "file index")
+
+class ImageDisplayer:
+ 
+    @staticmethod
+    def shuffle_files(file_names):
+        random.seed(int(time()))
+        random.shuffle(file_names)
+
+    def __init__(self, img_folder: str):
+        static_path = Path("static", img_folder)
+        self.image_paths = get_files_in_folder(static_path, '.png')
+
+        print("Files: ", self.image_paths)
+
+        self.shuffle_files(self.image_paths)
+        self.image_meta = get_files_in_folder(static_path, '.csv')[0]
+        self.lhs_image = self.get_file_index(0)
+        self.rhs_image = self.get_file_index(1)
+
+    def get_file_index(self, index):
+        return FileIndex(self.image_paths[index], index)
+    
+    def get_next_file_index(self):
+        return self.get_file_index(max(self.lhs_image.index, self.rhs_image.index) + 1)
+
+    def log_event(self):
+        pass
+
+    def get_curr_lhs_image(self):
+        return self.lhs_image.file
+
+    def get_curr_rhs_image(self):
+        return self.rhs_image.file
+
+    def change_lhs_image(self):
+        self.lhs_image = self.get_next_file_index()
+
+    def change_rhs_image(self):
+        self.rhs_image = self.get_next_file_index()
+
+
+global image_displayer
+image_displayer = ImageDisplayer("images")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         selected_image = request.form['selected_image']
-        if selected_image in current_images:
-            current_images.remove(selected_image)
-            current_images.append(random.choice(image_urls))
-    return render_template('index.html', image1=current_images[0], image2=current_images[1])
+        if selected_image == "LHS":
+            image_displayer.change_rhs_image()
+        else:
+            image_displayer.change_lhs_image()
+    return render_template('index.html', image_lhs=image_displayer.get_curr_lhs_image(), image_rhs=image_displayer.get_curr_rhs_image())
 
 if __name__ == '__main__':
     app.run(debug=True)
